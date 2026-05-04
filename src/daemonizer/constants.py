b66550259a76bbbe3b78e2ce6c7c19e1bbb774db
lcsrodriguez <lucasrodriguez.08@outlook.com>
2026-05-04 20:15:14 +0200

    Adding app name, app version in constants.py

--- src/daemonizer/constants.py
+++ src/daemonizer/constants.py
@@ -1,7 +1,16 @@
-"""Constants"""
+"""List of defined constants to be used in the project"""
 
-from typing import List
+from importlib.metadata import version
+from typing import List, Tuple
 
+APP_NAME: str = "daemonizer"
+
+# Version dynamically updated via Makefile targets
+APP_VERSION: str = version(APP_NAME)
+APP_VERSION_TUPLE: Tuple[int, ...] = tuple(map(int, APP_VERSION.split(".")))
+
+
+# UNIX system names
 UNIX_SYSTEM_NAMES: List[str] = [
     "Linux",
     "Darwin",
