736e62272a3699ec663e85f9e65b208c7f8c9c2f
lcsrodriguez <lucasrodriguez.08@outlook.com>
2026-05-06 21:21:25 +0200

    Adding logic with forever_loop for daemon def

--- sandbox/sample_daemon.py
+++ sandbox/sample_daemon.py
@@ -3,6 +3,7 @@
 import datetime
 import time
 
+from daemonizer.core.daemons.logic import forever_loop
 from daemonizer.core.daemons.unix import UNIXDaemon
 from daemonizer.core.handlers.base_handler import handler
 from daemonizer.utils.logs import get_logger, setup_logger
@@ -16,22 +17,24 @@ class SampleDaemon(UNIXDaemon):
     Sandbox daemon
     """
 
+    # TODO: Adding delay between each iteration
+    @forever_loop(catch_exceptions=False, after_delay=0.01)
     def run(self) -> None:
         """
         Function **run**
         :return: Nothing
         :rtype: None
         """
-        while True:
-            with open("/tmp/TOTO.log", "a") as f:
-                f.write(f"Hello, Lucas {datetime.datetime.now()}\n")
-                f.write(f"{self.get_arguments()['kwargs'].get('ssa')}\n")  # type: ignore[union-attr]
-            time.sleep(1)
-            self.logger.info("Sample Daemon")
+        # while True:
+        with open("/tmp/TOTO.log", "a") as f:
+            f.write(f"Hello, Lucas {datetime.datetime.now()}\n")
+            f.write(f"{self.get_arguments()['kwargs'].get('ssa', None)}\n")  # type: ignore[union-attr]
+        time.sleep(1)
+        # self.logger.info("Sample Daemon")
 
 
 if __name__ == "__main__":
     # with DaemonHandler() as h:
     #    h.add(daemon)
     #    h.run()
-    handler(SampleDaemon(name="TOTO", ssa="e"))
+    handler(SampleDaemon(name="e"))
