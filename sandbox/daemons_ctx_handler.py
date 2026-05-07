5d27941eb73f74eb4f9302cd2aa47fcf2c9f29e5
lcsrodriguez <lucasrodriguez.08@outlook.com>
2026-05-07 12:01:55 +0200

    Adding procedure on README.md + Adding clean daemons_ctx_handler.py

--- sandbox/daemons_ctx_handler.py
+++ sandbox/daemons_ctx_handler.py
@@ -2,13 +2,14 @@
 
 from daemonizer.core.handlers.ctx_manager import DaemonHandler
 from daemonizer.samples.writers import SampleDaemon1, SampleDaemon2
-from daemonizer.utils.logs import get_logger
+from daemonizer.utils.logs import get_logger, setup_logger
 
+setup_logger()
 logger = get_logger(__name__)
 
 
-if __name__ == "__main__":
-    # Daemons handler
-    with DaemonHandler() as h:
-        h.add(SampleDaemon1(name="daemon_1"))
-        h.add(SampleDaemon2(name="daemon_2"))
+# if __name__ == "__main__":
+# Daemons handler
+with DaemonHandler() as h:
+    h.add(SampleDaemon1(name="daemon_1"))
+    h.add(SampleDaemon2(name="daemon_2"))
