6c65fcfcabb91d874c74699c7241a55854c7541f
lcsrodriguez <lucasrodriguez.08@outlook.com>
2026-05-16 12:36:17 +0200

    Adding status CLI target

--- src/daemonizer/cli/commands.py
+++ src/daemonizer/cli/commands.py
@@ -6,7 +6,7 @@ import click
 
 from daemonizer.cli.processor import _cli_parse_daemons
 from daemonizer.constants import APP_NAME, APP_VERSION
-from daemonizer.core.daemons.flags import RESTART, START, STOP
+from daemonizer.core.daemons.flags import RESTART, START, STATUS, STOP
 from daemonizer.utils.logs import get_logger
 
 logger = get_logger(__name__)
@@ -122,3 +122,33 @@ def restart(script: str, daemons: Tuple[str, ...], strict: bool) -> None:
     :rtype: None
     """
     _cli_parse_daemons(script, list(daemons), RESTART, strict)
+
+
+# Command: $ daemonizer status
+@cli.command()
+@click.argument(
+    "script",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+    ),
+    required=True,
+)
+@click.argument("daemons", type=click.STRING, required=False, nargs=-1)
+@click.option(
+    "--strict/--no-strict",
+    "-s",
+    type=click.BOOL,
+    is_flag=True,
+    required=False,
+    default=True,
+)
+def status(script: str, daemons: Tuple[str, ...], strict: bool) -> None:
+    """
+    Restart daemons (CLI target)
+    :return: Nothing
+    :rtype: None
+    """
+    _cli_parse_daemons(script, list(daemons), STATUS, strict)
